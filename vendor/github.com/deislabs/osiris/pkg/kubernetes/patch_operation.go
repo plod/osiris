82086004a38d3df8439c21b40e4d45e11879b570
Daniel Morgan <daniel.morgan@nielsen.com>
2021-03-08 17:34:35 +0000

    moved paths

--- vendor/github.com/deislabs/osiris/pkg/kubernetes/patch_operation.go
+++
@@ -1,9 +0,0 @@
-package kubernetes
-
-// PatchOperation represents a discreet change to be applied to a Kubernetes
-// resource
-type PatchOperation struct {
-	Op    string      `json:"op"`
-	Path  string      `json:"path"`
-	Value interface{} `json:"value,omitempty"`
-}
--- vendor/github.com/deislabs/osiris/pkg/metrics/proxy_request_count.go
+++
@@ -1,7 +0,0 @@
-package metrics
-
-type ProxyConnectionStats struct {
-	ProxyID           string `json:"proxyId"`
-	ConnectionsOpened uint64 `json:"connectionsOpened"`
-	ConnectionsClosed uint64 `json:"connectionsClosed"`
-}
